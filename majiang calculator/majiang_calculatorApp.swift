9b88ca26a4bf97f2e66712162a3741cdfde9685d
Feiyu Chang <alanfeiyuchang@gmail.com>
2026-03-17 17:32:20 -0700

    Initial Commit

---
+++ majiang calculator/majiang_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  majiang_calculatorApp.swift
+//  majiang calculator
+//
+//  Created by ChangFeiyu on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct majiang_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
